146117fafc921c10904a0559929fd3ffe8ec26f0
JensonManning <157075196+JensonManning@users.noreply.github.com>
2024-12-08 10:46:04 -0700

    -- tailwind and primeng plugin done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { PrimeNG } from 'primeng/config';
+import { AccordionModule } from 'primeng/accordion';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, AccordionModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'RPM-FRONT-PRIME';
+  constructor(
+    private primeng : PrimeNG,
+    private http: HttpClient
+  ) {
+    this.primeng.ripple.set(true);
+  }
 }
